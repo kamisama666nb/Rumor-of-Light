9a9af6b9bf1d79d44f832c35f81605ee1ff96bb2
Aozorami <148611817+kamisama666nb@users.noreply.github.com>
2026-03-28 23:04:45 +0800

    Update Color.kt

--- app/src/main/java/com/rumoroflight/ui/theme/Color.kt
+++ app/src/main/java/com/rumoroflight/ui/theme/Color.kt
@@ -2,8 +2,10 @@ package com.rumoroflight.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-[span_3](start_span)// 文艺简约配色方案[span_3](end_span)
-val SoftBlueGrey = Color(0xFF78909C)
-val WarmBeige = Color(0xFFF5F5DC)
-val ElegantGreen = Color(0xFFE8F5E9)
-val MidnightCharcoal = Color(0xFF263238)
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
