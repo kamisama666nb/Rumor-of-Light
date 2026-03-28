4a44ba132c8f45f5275838c542cb8238c0abd0d0
Aozorami <148611817+kamisama666nb@users.noreply.github.com>
2026-03-28 23:36:29 +0800

    Update Type.kt

--- app/src/main/java/com/rumoroflight/ui/theme/Type.kt
+++ app/src/main/java/com/rumoroflight/ui/theme/Type.kt
@@ -1,9 +1,8 @@
 package com.rumoroflight.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font         // 必须有这一行
+import androidx.compose.ui.text.font.Font        // 只保留这一个 Font 导入
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
