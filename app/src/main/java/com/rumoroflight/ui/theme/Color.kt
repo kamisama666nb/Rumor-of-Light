3dc570c3459320104052783ffa1ef2c7b45ff9eb
Aozorami <148611817+kamisama666nb@users.noreply.github.com>
2026-03-28 20:25:28 +0800

    Create Color.kt

---
+++ app/src/main/java/com/rumoroflight/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.rumoroflight.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+[span_3](start_span)// 文艺简约配色方案[span_3](end_span)
+val SoftBlueGrey = Color(0xFF78909C)
+val WarmBeige = Color(0xFFF5F5DC)
+val ElegantGreen = Color(0xFFE8F5E9)
+val MidnightCharcoal = Color(0xFF263238)
